8c9248c9489057e13cd0494515d1f354d46df264
naveen <neonew8984@gmail.com>
2021-06-04 13:59:33 +0530

    rest api using node , express , and mongodb

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const Subscriber = require('../models/subscriber');
 
 // Getting all
 
-router.get('/', (req, res) => {
-    res.send('Hello World');
+router.get('/', async (req, res) => {
+    try {
+        const subscribers = await Subscriber.find();
+        res.json(subscribers);
+    } catch (err) {
+        res.status(500).json({message: err.message});
+    }
 });
 
 // Getting One
