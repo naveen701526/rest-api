e3c4547f3495d5114efe80ce3991346511a77022
naveen <neonew8984@gmail.com>
2021-06-04 13:54:53 +0530

    rest api using node , express , and mongodb

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -3,10 +3,14 @@ const router = express.Router();
 
 // Getting all
 
-router.get('/', (req, res) => {});
+router.get('/', (req, res) => {
+    res.send('Hello World');
+});
 
 // Getting One
-router.get('/:id', (req, res) => {});
+router.get('/:id', (req, res) => {
+    res.send(req.params.id);
+});
 // Creating One
 router.post('/', (req, res) => {});
 // Updating One
